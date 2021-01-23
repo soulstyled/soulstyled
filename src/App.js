f7b3944e41cc8fe00e7a226524739ec5a9f2f84d
Michael Geissler <michael@soulstyled.de>
2021-01-23 17:51:00 +0100

    fixed a typo

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <div className="content-card">
       <h1>Hey there,</h1>
-        <p>my name is <strong>Michael.</strong> I know a lot about creating, optimizing and maintaining websites of any scale and type.</p>
+        <p>my name is <strong>Michael.</strong> I know a lot about creating, optimizing and maintaining websites of any type and scale.</p>
         <p>I am currently leading a team at <b><a href="https://www.clickconcepts.de" target="_blank" rel="noopener noreferrer">clickconcepts</a></b>,
-        transforming an sluggish ecommerce monolith into something highly scaleable based on <b>Shopify</b> and <b>Frontastic</b>.<br /></p>
+        transforming an sluggish ecommerce stack into something highly scaleable based on <b>Shopify</b> and <b>Frontastic</b>.<br /></p>
         <p><i>Wanna talk?</i> <a href="https://twitter.com/soulstyled_de" target="_blank" rel="noopener noreferrer">@soulstyled_de on twitter</a>.</p>
       </div>
       <Portrait />
